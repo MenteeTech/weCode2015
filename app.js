1b73754ddf0c85479f9c7e61665a2356ae205285
Mikky Cecil <mikkycecil@gmail.com>
2015-07-25 12:55:41 -0700

    added template engine.

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // The global datastore for this example
 var listings;
 
+// Template engine (source: http://expressjs.com/advanced/developing-template-engines.html)
+var fs = require('fs'); // this engine requires the fs module
+app.engine('ntl', function (filePath, options, callback) { // define the template engine
+  fs.readFile(filePath, function (err, content) {
+    if (err) return callback(new Error(err));
+    // this is an extremely simple template engine
+    var rendered = content.toString().replace('#title#', '<title>'+ options.title +'</title>')
+    .replace('#message#', '<h1>'+ options.message +'</h1>');
+    return callback(null, rendered);
+  })
+});
+app.set('views', './views'); // specify the views directory
+app.set('view engine', 'ntl'); // register the template engine
+
 // Asynchronously read file contents, then call callbackFn
 function readFile(filename, defaultData, callbackFn) {
   fs.readFile(filename, function(err, data) {
